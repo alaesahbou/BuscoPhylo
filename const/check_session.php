6ee2111d87f7a3bb330aaa6bc66a2c86e1a28969
alae-eddine sahbou <60272832+alaesahbou@users.noreply.github.com>
2022-08-16 15:17:35 +0100

    ..

--- const/check_session.php
+++ const/check_session.php
@@ -1,4 +1,7 @@
 <?php
+if(!isset($user_id)){
+  $user_id==""
+}
 if(!isset($_COOKIE["__logged"])) {
 $res = "0";
 } else {
--- const/my_profile.php
+++ const/my_profile.php
@@ -2,7 +2,9 @@
 try {
 $conn = new PDO('mysql:host='.DBHost.';dbname='.DBName.';charset='.DBCharset.';collation='.DBCollation.';prefix='.DBPrefix.'', DBUser, DBPass);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
+if(!isset($user_id)){
+  $user_id==""
+}
 
 $stmt = $conn->prepare("SELECT * FROM tbl_reviews WHERE user = ?");
 $stmt->execute([$user_id]);
