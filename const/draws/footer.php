a93cfdcb84fd4c27401e24551e4d5f01398108db
alae-eddine sahbou <60272832+alaesahbou@users.noreply.github.com>
2022-08-16 15:03:19 +0100

    fix bugs

--- const/draws/footer.php
+++ const/draws/footer.php
@@ -1,3 +1,7 @@
+<?php
+error_reporting(0);
+ini_set('display_errors', 0);
+?>
 <footer class="footer">
   <div class="container">
     <div class="row">
--- const/draws/header.php
+++ const/draws/header.php
@@ -1,3 +1,7 @@
+<?php
+error_reporting(0);
+ini_set('display_errors', 0);
+?>
 <header class="header header--static">
   <div class="container">
     <div class="row">
